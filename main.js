beaeec8f4f5326283bc01394cd7febfdab80ea28
Oleksandr2207 <osponomarenko09@gmail.com>
2025-12-11 20:49:58 +0200

    kate

---
+++ main.js
@@ -0,0 +1,10 @@
+const icon = document.getElementById("icon")
+
+icon.addEventListener('click', function () {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        icon.src ="./img/sun.png" 
+    } else {
+        icon.src="./img/moon.png" 
+    }
+});
